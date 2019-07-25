ea34c3e65dd880a2d227a53fe3172c3a40c06231
Michiru <starffhwxx@gmil.com>
2019-07-25 11:24:31 +0800

    セル登録完了

--- study.app0725/BeginViewController.swift
+++ study.app0725/BeginViewController.swift
@@ -8,40 +8,35 @@
 
 import UIKit
 
-class BeginViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+class BeginViewController: UIViewController{
     
-    let Before = ["すうがくの基礎","アルファベットとフェニックス","始める前に"]
-    
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Before.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        cell.textLabel!.text = Before[indexPath.row]
-        
-        return cell
-        
-        
-    }
-    
-    
-    @IBOutlet weak var tableView: UITableView!
+   @IBOutlet weak var tableView: UITableView!
+    let beforeBegin = ["すうがくの基礎","アルファベットとフェニックス","始める前に"]
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 //おまじないはviewdidroadにかく
-        tableView.delegate = self
+    tableView.delegate = self
         tableView.dataSource = self
         
-        // Do any additional setup after loading the view.
     }
     
 
     
+}
+
+extension BeginViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return beforeBegin.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = beforeBegin[indexPath.row]
+        
+        return cell
+}
+
 }
